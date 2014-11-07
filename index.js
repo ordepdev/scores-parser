2cdae897ab9a57738e7f276e8940582dc1d46104
ordepdev <pedrotavares.dev@gmail.com>
2014-11-07 01:30:48 +0000

    parser works

---
+++ index.js
@@ -0,0 +1,64 @@
+var ansi    = require('ansi-styles');
+var http    = require('http');
+var pkg     = require('./package');
+var through = require('through');
+var trumpet = require('trumpet')();
+
+var elements = [],
+    json     = [];
+
+function push (buf) {
+  var data = buf.toString().trim();
+  if (data !== '') {
+    var imgIndex = data.indexOf('&');
+    if (imgIndex > -1) {
+      data = data.substr(0, imgIndex) + "'";
+    }
+    elements.push(data);
+  }
+};
+
+parseResults = function (cb) {
+  trumpet.selectAll('.fs *', function(el) {
+    el.createReadStream().pipe(el.createWriteStream({outer: true}));
+  });
+
+  trumpet.selectAll('.fd *', function(el) {
+    el.createReadStream().pipe(el.createWriteStream({outer: true}));
+  });
+
+  trumpet.selectAll('.fd', function (row) {
+    row.createReadStream().pipe(through(push));
+  });
+
+  trumpet.selectAll('.fs', function (row) {
+    row.createReadStream().pipe(through(push));
+  });
+
+  trumpet.selectAll('.fh', function (row) {
+    row.createReadStream().pipe(through(push));
+  });
+
+  trumpet.selectAll('.fa', function (row) {
+    row.createReadStream().pipe(through(push));
+  });
+
+  trumpet.once('end', function () {
+    for (var i = 0; i < elements.length; i+=4) {
+      json.push({
+        status: elements[i],
+        home:   elements[i+1],
+        result: elements[i+2],
+        away:   elements[i+3]
+      });
+    };
+
+    cb(json);
+  });
+
+  http.get(pkg.feed, function(res) {
+    var res = res.pipe(trumpet);
+  });
+}
+
+module.exports = parseResults;
---
+++ test.js
@@ -0,0 +1,13 @@
+var scores = require('./');
+var should = require('should');
+
+describe('scores-parser', function(){
+  describe('#parseResults()', function(){
+    it('should have items', function(done){
+      scores(function(data) {
+        data.should.have.not.length(0);
+        done();
+      });
+    });
+  });
+});
